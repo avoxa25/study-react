76afe4d77e27af0a6710428249428eee7d68b690
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-10 22:33:42 +0300

    Add didMount and willUnmount, setState

--- src/components/clock/clock.tsx
+++ src/components/clock/clock.tsx
@@ -3,12 +3,25 @@ import * as React from 'react';
 export default class Clock extends React.Component<{}> {
   public props: any;
   public state: any;
+  private timer?: NodeJS.Timer;
 
   constructor(props: any) {
     super(props);
 
     this.props = props;
-    this.state = {date: new Date()}
+    this.state = { date: new Date() }
+  }
+
+  public componentDidMount(): void {
+    this.timer = setInterval(() => this.launchClock(), 1000);
+  }
+
+  public componentWillUnmount(): void {
+    if (this.timer) clearInterval(this.timer);
+  }
+
+  private launchClock(): void {
+    this.setState({ date: new Date() });
   }
 
   render(): JSX.Element {
