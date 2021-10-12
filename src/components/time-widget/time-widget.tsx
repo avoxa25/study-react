450b16a06f08411887c3c3a10c592d4994b0dffe
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-12 13:49:36 +0300

    Add self-updating array of elapsed time

--- src/components/time-widget/time-widget.tsx
+++ src/components/time-widget/time-widget.tsx
@@ -6,6 +6,7 @@ import TimeCard from '../time-card/time-card';
 export default class TimeWidget extends React.Component<{}> {
   public props: any;
   public state: any = {ms: 0};
+  private elapsedTime: Array<Date> = [];
 
   constructor(props: any) {
     super(props);
@@ -16,13 +17,19 @@ export default class TimeWidget extends React.Component<{}> {
 
   public handleTimeChange(ms: number): void {
     this.setState({ms: ms});
+    this.elapsedTime.push(new Date(ms));
   }
 
   render(): JSX.Element {
+    let elapsedTime = this.elapsedTime.map((item, index) => <li key={index}>{item.toLocaleTimeString()}</li>);
+
     return (
       <section>
         <Clock onTimeChange={this.handleTimeChange} />
         <TimeCard timeInMs={this.state.ms}/>
+        <ul>
+          {elapsedTime}
+        </ul>
       </section>
     )
   }
