63314311236a3e6e3c42e528e691161fa0464627
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-12 12:16:45 +0300

    Add time card component

---
+++ src/components/time-card/time-card.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class TimeCard extends React.Component<{}> {
+    render(): JSX.Element {
+        return (<div>
+            Time Card!
+        </div>)
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import * as ReactDOM from 'react-dom';
 import './global-styles.scss';
 
 import Clock from './components/clock/clock';
+import TimeCard from './components/time-card/time-card';
 
 ReactDOM.render(
   <React.StrictMode>
     <Clock test="MSC" />
+    <TimeCard />
   </React.StrictMode>,
   document.getElementById('root')
 );
