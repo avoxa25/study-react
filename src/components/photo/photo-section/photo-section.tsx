f846795fdbf58e6e578f282003ca247d4adc2b5a
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-10-13 14:32:21 +0300

    Add photo section component

---
+++ src/components/photo/photo-section/photo-section.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default function PhotoSection(): JSX.Element {
+    return (<section>PHOTO IS HERE!</section>);
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import * as ReactDOM from 'react-dom';
 
 import './global-styles.scss';
 
-import TimeWidget from './components/time-widget/time-widget';
+import TimeWidget from './components/time/time-widget/time-widget';
+import PhotoSection from './components/photo/photo-section/photo-section';
 
 ReactDOM.render(
   <React.StrictMode>
     <TimeWidget />
+    <PhotoSection/>
   </React.StrictMode>,
   document.getElementById('root')
 );
